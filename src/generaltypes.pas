9b10fe4db77d6decffcdb3f3c4ff053e456f9ced
icefairy64 <ishido.uu@gmail.com>
2014-08-21 08:19:46 +0400

    Another inital commit

---
+++ src/generaltypes.pas
@@ -0,0 +1,45 @@
+unit generaltypes;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, Sysutils;
+
+type
+  PTag = ^TTag;
+  PUser = ^TUser;
+  PPost = ^TPost;
+
+  TTag = record
+    ID: LongWord;
+    Name: String;
+  end;
+
+  TUser = record
+    ID: LongWord;
+    Name: String;
+    Score: LongInt;
+    Favs: array of PPost;
+  end;
+
+  TPost = record
+    ID: LongWord;
+    Title: String;
+    Tags: array of PTag;
+    ThumbURL: String;
+    ThumbFile: String;
+    SrcURL: String;
+    SrcFile: String;
+    Uploader: PUser;
+    Module: String;
+    Approved: Boolean;
+    Favs: LongWord;
+    Score: LongInt;
+  end;
+
+implementation
+
+end.
+
---
+++ src/icebooru.lpr
@@ -0,0 +1,19 @@
+
+{           -=< icebooru >=-            }
+{ Yet another xbooru-like image gallery }
+{         Written by icefairy64         }
+
+program icebooru;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes, generaltypes, simpleipc;
+
+begin
+
+end.
+
